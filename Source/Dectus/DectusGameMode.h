1a8980e4d1e9de8a215f6220cca5893c3f3387b5
Emil <emiltan97@hotmail.com>
2023-05-24 19:40:49 +1000

    Initial commit

---
+++ Source/Dectus/DectusGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DectusGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADectusGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADectusGameMode();
+};
+
+
+
